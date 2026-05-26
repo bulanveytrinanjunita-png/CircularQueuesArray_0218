e7e03bdeea33380d115a48f9d1b2ddf146d5416e
Veytrinanjunita <bulanveytrinanjunita@gmail.com>
2026-05-26 10:49:03 +0700

    display jika FRONT <= REAR, iterasi dari FRONT hingga akhir array

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -96,5 +96,16 @@ public:
             }
             cout << endl;
         }
+        else
+        {
+            // jika FRONT <= REAR, iterasi dari FRONT hingga akhir array
+            while (FRONT_position <= max - 1)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+
+            FRONT_position = 0;
+        }
     }
 };
